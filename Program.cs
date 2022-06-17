90ee307d964cd4726c9968d905d6e42ed765ec7b
Jeffrey Lee <darkthread@gmail.com>
2022-06-17 20:29:39 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//https://weblog.west-wind.com/posts/2019/Jan/22/COM-Object-Access-and-dynamic-in-NET-Core-2x
+//https://github.com/RickStrahl/Westwind.Utilities
+using System.Reflection;
+var MemberAccess =
+    BindingFlags.Public | BindingFlags.NonPublic |
+    BindingFlags.Static | BindingFlags.Instance | BindingFlags.IgnoreCase;
+var type = Type.GetTypeFromProgID("InternetExplorer.Application");
+var ie = Activator.CreateInstance(type);
+ie.GetType().InvokeMember("Visible", MemberAccess | BindingFlags.SetProperty, null, 
+    ie, new object[1] { true });
+ie.GetType().InvokeMember("Navigate", MemberAccess | BindingFlags.InvokeMethod, null,
+    ie, new object[] { "about:blank" });
